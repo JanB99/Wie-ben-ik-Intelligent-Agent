1b898cb9d5d4dcae6527ed7bb34d5b7e87d1ce06
JanB <jan.baljan@hotmail.com>
2020-10-07 20:26:16 +0200

    api's toegevoegd

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 import main
 import csv
@@ -19,10 +19,53 @@ with open("dataset.csv", 'r', newline='') as file:
 tree = main.Tree(dataset)
 ai = tree.root
 
+playerCharacter = None
+name = ''
+
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify(tree.toJson())
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    global name
+    if request.args.get('name'):
+        name = request.args.get('name')
+
+    return jsonify(name)
+
+@app.route('/getAllCharacters', methods=['GET'])
+def characters():
+    return jsonify(dataset)
+
+@app.route('/character', methods=['GET', 'POST'])
+def setCharacter():
+    global playerCharacter
+
+    arg = request.args.get('num')
+    if arg and int(arg) < len(dataset) and int(arg) >= 0:
+        playerCharacter = dataset[int(arg)]
+    
+    return jsonify(playerCharacter)
+
+@app.route('/labels', methods=['GET'])
+def getLabels():
+    return jsonify(headers)
+
+@app.route('/values', methods=['GET', 'POST'])
+def getValues():
+
+    if 'label' in request.args:
+        return jsonify(list({x[headers.index(request.args.get('label'))] for x in dataset}))
+
+@app.route('/question', methods=['GET', 'POST'])
+def postQuestion():
+    pass
+
+@app.route('/image', methods=['GET'])
+def getImage():
+    return send_file('tijger.jpg', mimetype='image/jpg')
 
 @app.route('/next', methods=['GET', 'POST'])
 def ask():
