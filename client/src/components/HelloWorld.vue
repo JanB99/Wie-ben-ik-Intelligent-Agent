4ce89d581406060a2a545a847b1a07d7102793cc
JanB <jan.baljan@hotmail.com>
2020-10-07 10:41:24 +0200

    re init (foute mappenstructuur gefixt)

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    {{data}}
+    <h1>{{ data }}</h1>
+    
   </div>
 </template>
 
@@ -30,5 +31,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
