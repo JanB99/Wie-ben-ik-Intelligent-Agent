b930953b134735aa6dbba6bdd1c23fc344a9f84d
LucvanDeenen <Lucvd@hotmail.nl>
2020-10-13 10:30:01 +0200

    added theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,10 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {​​            ​​
+                primary: '#a7d9ed',
+                secondary: '#80cbde',
+                accent: '#0f7593',
+        
+    }​​,
 });
