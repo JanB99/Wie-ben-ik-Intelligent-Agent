d4b68d5a4d3361301b7a43767191a3d2c5dff628
LucvanDeenen <Lucvd@hotmail.nl>
2020-10-15 02:14:06 +0200

    fix deze gedoe want ja t doet t nog niet eigenlijk

--- client/src/components/DecisionTree.vue
+++ client/src/components/DecisionTree.vue
@@ -8,64 +8,95 @@
           v-bind:key="index"
           md5
         >
-          <v-layout class="d-flex justify-center">
-            <v-card
-              v-if="index == 0"
-              outline
-              block
-              class="secondary text-center white--text text--lighten-5 mx-5 pa-2"
-              column
-            >
-              <b>{{ getQuestion(index) }}</b>
-            </v-card>
+          <!-- Start question -->
+          <v-layout class="d-flex flex-column" v-if="index == 0">
+            <!-- Question -->
+            <v-layout class="d-flex justify-center">
+              <v-col cols="8" class="flex-grow-0 flex-shrink-0">
+                <v-card
+                  class="secondary text-center white--text text--lighten-5 pa-2"
+                  column
+                >
+                  <b>{{ getQuestion(index) }}</b>
+                </v-card>
+              </v-col>
+            </v-layout>
+
+            <!-- True or False -->
+            <v-layout class="d-flex justify-space-around pa-2 row">
+              <v-card
+                class="text-center white--text text--lighten-5 mx-2 pa-2"
+                :class="awnser ? 'accent' : 'secondary'"
+                column
+              >
+                <b>Ja</b>
+              </v-card>
+              <v-card
+                class="text-center white--text text--lighten-5 mx-2 pa-2"
+                :class="!awnser ? 'accent' : 'secondary'"
+                column
+              >
+                <b>Nee</b>
+              </v-card>
+            </v-layout>
           </v-layout>
 
-          <div v-if="index != 0" column>
-            <v-layout class="d-flex justify-center">
-              <v-card
-                v-if="decisions[index - 1] == true"
-                outline
-                block
-                class="secondary text-center white--text text--lighten-5 mx-5 pa-2"
-                column
-              >
-                <b>{{ getQuestion(index) }}</b>
-              </v-card>
+          <!-- Next questions -->
+          <v-layout v-if="index != 0">
+            <!-- True side -->
+            <v-layout class="d-flex column align-center mt-2">
+              <!-- Questions -->
+              <v-col cols="8" class="flex-grow-0 flex-shrink-0">
+                <v-card
+                  align-start
+                  class="text-center white--text text--lighten-5 mx-2 pa-2"
+                  :class="awnser == true ? 'secondary' : 'grey'"
+                  column
+                >
+                  <b>{{ getQuestion(index, true) }}</b>
+                </v-card>
+              </v-col>
+
+              <!-- Awnsers -->
+              <v-layout class="justify-center pa-2">
+                <!-- True -->
+                <v-card
+                  class="text-center white--text text--lighten-5 mx-2 pa-2"
+                  :class="awnser ? 'accent' : 'secondary'"
+                  column
+                >
+                  <b>Ja</b>
+                </v-card>
+              </v-layout>
             </v-layout>
 
-            <v-layout class="d-flex justify-center">
-              <v-card
-                v-if="decisions[index - 1] == false"
-                outline
-                block
-                class="secondary text-center white--text text--lighten-5 mx-5 pa-2"
-                column
-              >
-                <b>{{ getQuestion(index) }}</b>
-              </v-card>
-            </v-layout>
-          </div>
+            <!-- False side -->
+            <v-layout class="d-flex column align-center mt-2">
+              <!-- Questions -->
+              <v-col cols="8" class="flex-grow-0 flex-shrink-0">
+                <v-card
+                  block
+                  class="text-center white--text text--lighten-5 mx-2 pa-2"
+                  :class="awnser == false ? 'secondary' : 'grey'"
+                  column
+                >
+                  <b>{{ getQuestion(index, false) }}</b>
+                </v-card>
+              </v-col>
 
-          <div class="d-flex justify-center mt-1 mb-3" column>
-            <v-card
-              outline
-              block
-              class="text-center white--text text--lighten-5 mx-2 pa-2"
-              :class="awnser ? 'accent' : 'secondary'"
-              column
-            >
-              <b>True</b>
-            </v-card>
-            <v-card
-              outline
-              block
-              class="text-center white--text text--lighten-5 mx-2 pa-2"
-              :class="!awnser ? 'accent' : 'secondary'"
-              column
-            >
-              <b>False</b>
-            </v-card>
-          </div>
+              <!-- Awnsers -->
+              <v-layout class="justify-center pa-2">
+                <!-- True -->
+                <v-card
+                  class="text-center white--text text--lighten-5 mx-2 pa-2"
+                  :class="!awnser ? 'accent' : 'secondary'"
+                  column
+                >
+                  <b>Nee</b>
+                </v-card>
+              </v-layout>
+            </v-layout>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
@@ -79,7 +110,7 @@ export default {
   data() {
     return {
       data: null,
-      decisions: [true, false, true, false, true],
+      decisions: [false, false, true, true],
     };
   },
 
@@ -88,23 +119,44 @@ export default {
   },
 
   methods: {
-    getQuestion(depth) {
+    getQuestion(depth, awnser) {
       let temp = this.data;
-      let question;
+      let parent = temp;
+      let question = this.data?.question;
 
-      for (let index = 0; index <= depth; index++) {
-        if (temp.question) {
-          question = temp.question;
-          if (this.decisions[index] == true) {
+      for (let i = 0; i <= depth; i++) {
+        if (temp?.question) {
+          if (depth == i && this.decisions[i] == awnser) {
+            question = temp.question;
+          }
+
+          if (depth == i && this.decisions[i] != awnser) {
+            if (awnser == true) {
+              question = parent.true_branch.question;
+              console.log(question, 'is true pik', this.decisions[i], awnser, i);
+            }
+            if (awnser == false) {
+              question = parent.false_branch.question;
+              console.log(question, 'is false pik', this.decisions[i], awnser, i);
+            }
+          }
+
+          parent = temp;
+
+          // If True
+          if (this.decisions[i]) {
             temp = temp.true_branch;
           }
-          if (this.decisions[index] == false) {
+
+          // If False
+          if (!this.decisions[i]) {
             temp = temp.false_branch;
           }
         } else {
-          return temp.prob
+          return temp?.prob;
         }
       }
+
       return question;
     },
 
